6cbb86f8d0331ae455d6680cd6b4ecd8fb8a1881
AaronElijah <aaronzakelijah@googlemail.com>
2026-03-03 22:46:23 +0000

    fix: imx: cortexa7: move botblox, ruggedsom device to cortexa7 subtarget
    - Fix target/linux builds for imx platforms

--- target/linux/imx/image/cortexa7.mk
+++ target/linux/imx/image/cortexa7.mk
@@ -48,12 +48,9 @@ define Device/Default
   PROFILES := Default
   FILESYSTEMS := squashfs ext4
   KERNEL_INSTALL := 1
-  KERNEL_SUFFIX := -uImage
   KERNEL_NAME := zImage
-  KERNEL := kernel-bin | uImage none
-  KERNEL_LOADADDR := 0x80008000
+  KERNEL := kernel-bin
   DTS_DIR := $(DTS_DIR)/nxp/imx
-  IMAGES :=
 endef
 
 define Device/technexion_imx7d-pico-pi
@@ -66,8 +63,75 @@ define Device/technexion_imx7d-pico-pi
 	kmod-input-touchscreen-edt-ft5x06 kmod-usb-hid kmod-btsdio \
 	kmod-brcmfmac brcmfmac-firmware-4339-sdio cypress-nvram-4339-sdio
   FILESYSTEMS := squashfs
+  KERNEL := kernel-bin | uImage none
+  KERNEL_SUFFIX := -uImage
+  KERNEL_LOADADDR := 0x80008000
   IMAGES := combined.bin sysupgrade.bin
   IMAGE/combined.bin := append-rootfs | pad-extra 128k | imx-sdcard-raw-uboot
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 TARGET_DEVICES += technexion_imx7d-pico-pi
+
+# 4MB bootloader (hence 4MB offset)
+IMX_KERNELPART_OFFSET = 4
+#IMX_KERNELPART_SIZE = 40
+#IMX_ROOTFSPART_OFFSET = 64
+#IMX_IMAGE_SIZE = $(shell echo $$((($(IMX_ROOTFSPART_OFFSET) + $(CONFIG_TARGET_ROOTFS_PARTSIZE)))))
+
+define Build/imx-combined-image-prepare
+	rm -rf $@.boot
+	mkdir -p $@.boot
+endef
+
+define Build/imx-combined-image-clean
+	rm -rf $@.boot
+endef
+
+define Build/imx-combined-image
+	$(CP) $(IMAGE_KERNEL) $@.boot/$(KERNEL_NAME)
+
+	$(foreach dts,$(DEVICE_DTS), \
+		$(CP) \
+			$(DTS_DIR)/$(dts).dtb \
+			$@.boot/;
+	)
+
+	PARTOFFSET="$(IMX_KERNELPART_OFFSET)M" PADDING=1 \
+    $(if $(filter $(1),efi),GUID="$(IMG_PART_DISKGUID)") \
+    $(SCRIPT_DIR)/gen_image_generic.sh $@ \
+		$(CONFIG_TARGET_KERNEL_PARTSIZE) $@.boot \
+		$(CONFIG_TARGET_ROOTFS_PARTSIZE) $(IMAGE_ROOTFS) \
+		1024
+endef
+
+define Build/imx-no-uboot-no-spl
+    $(Build/imx-combined-image-prepare)
+	$(Build/imx-combined-image)
+    $(Build/imx-combined-image-clean)
+endef
+
+define Build/imx-uboot-no-spl
+	$(Build/imx-combined-image-prepare)
+	$(Build/imx-combined-image)
+	dd if=$(STAGING_DIR_IMAGE)/$(UBOOT)-u-boot.imx of=$@ bs=1024 seek=1 conv=notrunc
+	$(Build/imx-combined-image-clean)
+endef
+
+define Device/botblox_ruggedsom
+  $(call Device/Default)
+  DEVICE_VENDOR := BotBlox
+  DEVICE_MODEL := RUGGEDSOM
+  DEVICE_VARIANT := eMMC Boot
+  BOARD_NAME := RuggedSOM
+  DEVICE_PACKAGES += firmware-sdma u-boot-ruggedsom
+  UBOOT := ruggedsom
+  DEVICE_DTS := imx6ull-botblox-swr-som
+  KERNEL := kernel-bin
+  IMAGES := sysupgrade.bin emmc.img emmc-efi.img emmc-no-uboot.img emmc-no-uboot-efi.img
+  IMAGE/emmc.img := imx-uboot-no-spl
+  IMAGE/emmc-efi.img := imx-uboot-no-spl efi
+  IMAGE/emmc-no-uboot.img := imx-no-uboot-no-spl
+  IMAGE/emmc-no-uboot-efi.img := imx-no-uboot-no-spl efi
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += botblox_ruggedsom
